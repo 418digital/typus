54237c27ac99b54e1a1bd87d78e3a7e8d0d956d9
David Graham <david@welovetoruby.com>
2017-11-08 23:42:27 +0000

    Don’t attempt to .dup params when it’s a type of Nil. Fixes for Ruby 2.2.x

--- app/helpers/admin/resources_helper.rb
+++ app/helpers/admin/resources_helper.rb
@@ -3,7 +3,7 @@ module Admin::ResourcesHelper
   def search(resource = @resource, params = params)
     if (typus_search = resource.typus_defaults_for(:search)) && typus_search.any?
 
-      hidden_filters = params.dup
+      hidden_filters = params.nil? ? {} : params.dup
       rejections = %w(controller action locale utf8 sort_order order_by search page)
       hidden_filters.delete_if { |k, v| rejections.include?(k) }
 
